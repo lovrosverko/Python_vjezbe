fdcf79b9d436556b4b501c05885dbf9848d2d58b
Lovro Šverko <lovro.sverko@gmail.com>
2025-04-23 11:43:20 +0200

    Update zadatak_4.py
    Promjena zbog demonstracije uređivanja dokumenta.

--- Vježba 20/zadatak_4.py
+++ Vježba 20/zadatak_4.py
@@ -4,7 +4,6 @@ def jedinstveni_elementi(lista):
         if lista.count(element) > 1:
             lista.remove(element)
     
-    
     return lista
 
 print(jedinstveni_elementi([1, 2, 2, 3, 3, 3, 4, 5, 5])) # Treba ispisati: [1, 2, 3, 4, 5]
